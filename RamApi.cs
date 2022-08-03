dc189dcffd2cc6a036298f5b777d64b0b54d959f
Gamearoo Codes <gamearoo10@gmail.com>
2022-08-02 23:28:20 -0400

    Added helloAsync

---
+++ RamApi.cs
@@ -0,0 +1,31 @@
+﻿using DalSoft.RestClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamApidotnet
+{
+    
+    public class RamApi
+    {
+        private static string APIURL = $"https://api.rambot.xyz";
+        public static async Task<dynamic> helloAsync(string ApiKey, string Version, string lang = "english")
+        {
+            string requestUrl = APIURL + $"/{Version}/public/hello/{lang}";
+
+            dynamic client = new RestClient(requestUrl, new Config().UseRetryHandler(
+                 maxRetries: 5,
+                    waitToRetryInSeconds: 6,
+                    maxWaitToRetryInSeconds: 10,
+                    backOffStrategy: DalSoft.RestClient.Handlers.RetryHandler.BackOffStrategy.Linear
+                ));
+
+            return await client.Headers(new Dictionary<string, string> { { "api-key", ApiKey } }).Get();
+        }
+    }
+
+
+
+}
